9ee8beda0d03a9a3969dda03bac0c899ff60d81c
Huy Nguyen <huyb.1991@gmail.com>
2019-03-24 16:25:41 +0700

    Remove default createdAt and updatedAt fields for models

--- migrations/20190324030648-create-author.js
+++ migrations/20190324030648-create-author.js
@@ -17,14 +17,6 @@ module.exports = {
       },
       avatar: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
--- migrations/20190324031013-create-post.js
+++ migrations/20190324031013-create-post.js
@@ -18,14 +18,6 @@ module.exports = {
       authorId: {
         allowNull: false,
         type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
